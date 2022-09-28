9613670e2418a8fef61735f0168f631f09999382
Hexa56 <testdemo6956@gmail.com>
2022-09-28 19:14:05 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const app = express();
+const http = require('http')
+const {Server} = require("socket.io")
+const cors = require("cors");
+
+app.use(cors());
+const server = http.createServer(app)
+const io = new Server(server, {
+    cors:{
+        origin: "http://localhost:3000",
+        methods: ['GET', 'POST']
+    }
+});
+let Users = [];
+io.on('connection', (socket) => {
+    console.log(`User Id ${socket.id}`);
+    socket.emit("user_id",socket.id)
+    socket.on("join", (data) => {
+        socket.join(data);
+    })
+
+    socket.on("send_message", (data) => {
+        console.log(data)
+        socket.to(data.room).emit("recived_message", data)
+    })
+})
+
+server.listen(3001, ()=>{
+    console.log("server is running")
+})
